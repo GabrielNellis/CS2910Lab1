aafb4a7c0c8ae40c4d8aecac6e63b8a063ff8ab0
5089401 <5089401@TAG030588>
2025-01-16 14:44:43 -0700

    Add project files.

---
+++ CS2910Lab1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <fstream>
+int main()
+{
+    
+}
+//student format is in the form of student#, l.name, f.name, student #, email
+//grades format is in the form of l.name, f.name, grades (based on course#)
+//courses format is in the form of courseID, when, course#
